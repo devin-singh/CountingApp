3bc00104c8c602d66647336e24b9ad0ec679b366
Devin Ghumman <dghumman19@gmail.com>
2022-06-13 18:46:45 -0700

    Counting App Kotlin basics

---
+++ app/src/main/java/devin/singh/helloworld/MainActivity.kt
@@ -0,0 +1,24 @@
+package devin.singh.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val textView = findViewById<TextView>(R.id.textView)
+        var timesClicked: Int = 0
+        textView.text = "0"
+        btnClickMe.setOnClickListener {
+            timesClicked++
+            textView.text = timesClicked.toString()
+            Toast.makeText(this, "Hey I'm a toast!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
